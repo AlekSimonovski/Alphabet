0fd199d10597e8b9d149bad2c881803a61f9d880
Александар Симоновски <alek.simonovski02@gmail.com>
2023-04-02 21:17:19 +0200

    Added comments to code.

--- src/Azbuka.java
+++ src/Azbuka.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class Azbuka {
     public static void main(String[] args) {
-        Random random = new Random();
+        Random random = new Random();	// kreira slucajno generiran integer
         char bukva = (char) (random.nextInt(26) + 'a'); // izbira slucajna bukva
 
         Scanner tastatura = new Scanner(System.in);
         int obidi = 0;
         while (true) {
             System.out.print("Pogodi ja slucajnata bukva: ");
-            char izbor = tastatura.next().charAt(0);
+            char izbor = tastatura.next().charAt(0);	// vlez od strana na korisnik
             obidi++;
 
-            if (izbor == bukva) {
+            if (izbor == bukva) {	// proveruva dali vnesenata bukva e slucajno izbranata bukva
                 System.out.println("Chestito, ja pogodivte bukvata.");
                 break;
             } else {
